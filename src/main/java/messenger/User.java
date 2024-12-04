02f7453f8a605cd718e10e0fd80d215b8528ca20
Ildar <ildarsafa5@gmail.com>
2024-12-04 13:59:48 +0300

    сделал класс User| Сафиулин Ильдар

---
+++ src/main/java/messenger/User.java
@@ -0,0 +1,53 @@
+package messenger;
+
+public class User {
+    private String nickname;
+    private String name;
+    private String number;
+    private String password;
+
+    public User() {
+
+    }
+
+    public User(String name, String nickname, String password, String number) {
+        this.password = password;
+        this.number = number;
+        this.nickname = nickname;
+        this.name = name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+}
